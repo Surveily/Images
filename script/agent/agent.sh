cd5686a0382b1d9598b644127a97f6e0aa9399b8
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2020-08-28 12:03:39 +0200

    fix line end

--- script/agent/agent.sh
+++ script/agent/agent.sh
@@ -4,4 +4,5 @@
 
 docker pull surveily/developer.dotnet:3.1-build
 docker rm $1 -f
-docker run --privileged --name $1 -d --restart always --cpus=.66 -v /Users/service/.ssh:/root/.ssh -v $1-varunit:/etc/varunit -v /var/run/docker.sock:/var/run/docker.sock --env-file /Users/service/Agent/agent.env -e AGENT=$1 surveily/developer.dotnet:3.1-build
+docker volume rm $1-varunit
+docker run --privileged --name $1 -d --restart always --cpus=.66 -v /Users/service/.ssh:/root/.ssh -v $1-varunit:/etc/varunit -v /var/run/docker.sock:/var/run/docker.sock --env-file agent.env -e AGENT=$1 surveily/developer.dotnet:3.1-build
