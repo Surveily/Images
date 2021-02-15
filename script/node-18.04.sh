c57800f598c3f003c840b7fe0e279795db7cca3e
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2021-02-15 13:01:54 +0100

    Update node-18.04.sh

--- script/node-18.04.sh
+++ script/node-18.04.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 # Register Nvidia
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add - \
