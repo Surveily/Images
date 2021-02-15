fd4b4a069589f7f2e1b7033737e8d689d31197c0
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2021-02-15 13:02:05 +0100

    Update node-16.04.sh

--- script/node-16.04.sh
+++ script/node-16.04.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 # Register OpenVPN
 curl -s https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
 echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
