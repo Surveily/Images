e0709920f95c701c2d57efc74c5f4ce44b481c93
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2021-12-20 14:58:47 +0100

    light build agent

--- src/surveily.developer.dotnet.build.dockerfile
+++ src/surveily.developer.dotnet.build.dockerfile
@@ -1,51 +1,39 @@
-# Copyright (c) Surveily sp. z o.o. All rights reserved.
-# Remember to run with -v /var/run/docker.sock:/var/run/docker.sock
+FROM ubuntu:21.10
 
-ARG version=3.1
-ARG vsts=2.173.0
-ARG powershell=18.04
-FROM surveily/developer.dotnet:${version}-sdk
-
-ARG vsts
-ARG powershell
-
-# Settings
-ENV TZ=Etc/UTC
+# To make it easier for build and release pipelines to run apt-get,
+# configure apt to not require confirmation (assume the -y argument by default)
 ENV DEBIAN_FRONTEND=noninteractive
-ENV DOTNET_CLI_TELEMETRY_OPTOUT=1
+RUN echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
-WORKDIR /
+RUN apt-get update && apt-get upgrade -y \
+&& apt-get install -y --no-install-recommends \
+        ca-certificates \
+        curl \
+        jq \
+        git \
+        iputils-ping \
+        libcurl4 \
+        libunwind8 \
+        netcat \
+        libssl1.0 \
+		zip \
+		unzip \
+        ssh\
+&& rm -rf /var/lib/apt/lists/*
 
-# Update
-RUN sed 's/main$/main universe/' -i /etc/apt/sources.list && \
-    apt-get update && apt-get upgrade -y && \
-    # Apply Timezone
-    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone && \
-    # Install Dependencies 
-    apt-get install -y zip unzip build-essential gnupg-agent xorg libssl-dev libxrender-dev wget libxslt-dev libxml2-dev gdebi nodejs libc-dev libpng* gcc make autoconf libtool pkg-config nasm apt-transport-https ca-certificates curl software-properties-common libappindicator3-1 fonts-liberation libasound2 xdg-utils iputils-ping \
-    # Install Java
-    openjdk-8-jre openjdk-8-jdk && \
-    # Install Docker
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
-    apt-key fingerprint 0EBFCD88 && \
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-    apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io && \
-    # Install Agent
-    cd /root && \
-    wget https://vstsagentpackage.azureedge.net/agent/$vsts/vsts-agent-linux-x64-$vsts.tar.gz -O agent.tar.gz && \
-    tar -zxvf agent.tar.gz && \
-    rm agent.tar.gz && \
-    # Run SSH
-    mkdir ~/.ssh && \
+RUN curl -LsS https://aka.ms/InstallAzureCLIDeb | bash \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN curl https://get.docker.com | sh \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN mkdir ~/.ssh && \
     chmod 700 ~/.ssh && \
-    ssh-keyscan -t rsa github.com gitlab.com bitbucket.org ssh.dev.azure.com vs-ssh.visualstudio.com >> ~/.ssh/known_hosts && \
-    # Clean up
-    ldconfig && \
-    apt-get autoremove -y && \
-    apt-get clean -y && \
-    rm -rf /var/lib/apt/lists/*
+    ssh-keyscan -t rsa github.com gitlab.com bitbucket.org ssh.dev.azure.com vs-ssh.visualstudio.com >> ~/.ssh/known_hosts
 
-COPY src/surveily.developer.dotnet.build.sh /root/entrypoint.sh
-RUN chmod +x /root/entrypoint.sh
-WORKDIR /root
-ENTRYPOINT /root/entrypoint.sh
+WORKDIR /azp
+
+COPY src/surveily.developer.dotnet.build.sh .
+RUN chmod +x surveily.developer.dotnet.build.sh
+
+CMD ["./surveily.developer.dotnet.build.sh"]
--- src/surveily.developer.dotnet.build.sh
+++ src/surveily.developer.dotnet.build.sh
@@ -1,12 +1,113 @@
 #!/bin/bash
-
 set -e
 
-if [ ! -f "/etc/varunit/system-init" ]; then
-    ./config.sh --unattended --url $URL --auth pat --token $TOKEN --pool "$POOL" --agent $AGENT --replace --acceptTeeEula
-    touch /etc/varunit/system-init
+ARCHITECTURE="$(arch)"
+PLATFORM=$ARCHITECTURE
+if [[ $PLATFORM == x86_64 ]]; then
+  PLATFORM="linux-x64"
+elif [[ $PLATFORM == arm* ]]; then
+  PLATFORM="linux-arm"
+elif [[ $PLATFORM == aarch64 ]]; then
+  PLATFORM="linux-arm64"
+else 
+  echo 1>&2 "Unsupported architecture"
+  exit 1
 fi
 
-docker login $DOCKER_LOGIN.azurecr.io -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
+if [ -z "$AZP_URL" ]; then
+  echo 1>&2 "error: missing AZP_URL environment variable"
+  exit 1
+fi
 
-./run.sh
+if [ -z "$AZP_TOKEN_FILE" ]; then
+  if [ -z "$AZP_TOKEN" ]; then
+    echo 1>&2 "error: missing AZP_TOKEN environment variable"
+    exit 1
+  fi
+
+  AZP_TOKEN_FILE=/azp/.token
+  echo -n $AZP_TOKEN > "$AZP_TOKEN_FILE"
+fi
+
+unset AZP_TOKEN
+
+if [ -n "$AZP_WORK" ]; then
+  mkdir -p "$AZP_WORK"
+fi
+
+
+
+echo "Setup - Determining matching Azure Pipelines agent..."
+AZP_AGENT_RESPONSE=$(curl -LsS \
+  -u user:$(cat "$AZP_TOKEN_FILE") \
+  -H 'Accept:application/json' \
+  "$AZP_URL/_apis/distributedtask/packages/agent?platform=$PLATFORM")
+if echo "$AZP_AGENT_RESPONSE" | jq . >/dev/null 2>&1; then
+  AZP_AGENTPACKAGE_URL=$(echo "$AZP_AGENT_RESPONSE" \
+    | jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]')
+fi
+if [ -z "$AZP_AGENTPACKAGE_URL" -o "$AZP_AGENTPACKAGE_URL" == "null" ]; then
+  echo 1>&2 "Setup - Could not determine a matching Azure Pipelines agent. Check that account '$AZP_URL' is correct and the token is valid for that account"
+  exit 1
+fi
+echo "Setup - Latest agent package will be downloaded from $AZP_AGENTPACKAGE_URL"
+echo "Setup - Downloading and unpacking Azure Pipelines agent..."
+curl -LsS $AZP_AGENTPACKAGE_URL | tar -xz & wait $!
+echo "Setup - Completed download and unpack"
+
+
+
+
+export AGENT_ALLOW_RUNASROOT="1"
+
+cleanup() {
+  if [ -e config.sh ]; then
+    print_header "Cleanup. Removing Azure Pipelines agent..."
+
+    # If the agent has some running jobs, the configuration removal process will fail.
+    # So, give it some time to finish the job.
+    while true; do
+      ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+
+      echo "Retrying in 30 seconds..."
+      sleep 30
+    done
+  fi
+}
+
+print_header() {
+  lightcyan='\033[1;36m'
+  nocolor='\033[0m'
+  echo -e "${lightcyan}$1${nocolor}"
+}
+
+# Let the agent ignore the token env variables
+export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
+
+source ./env.sh
+
+print_header "1. Configuring Azure Pipelines agent..."
+
+cleanup;
+
+./config.sh --unattended \
+  --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+  --url "$AZP_URL" \
+  --auth PAT \
+  --token $(cat "$AZP_TOKEN_FILE") \
+  --pool "${AZP_POOL:-Default}" \
+  --work "${AZP_WORK:-_work}" \
+  --replace \
+  --acceptTeeEula & wait $!
+
+print_header "2. Running Azure Pipelines agent..."
+
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+# To be aware of TERM and INT signals call run.sh
+# Running it with the --once flag at the end will shut down the agent after the build is executed
+./run.sh "$@" &
+
+wait $!
