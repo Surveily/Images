ac2785cf454c05b625999425dd98d3425e52c60b
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2022-02-16 15:58:24 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.17/amd64/linux-headers-5.10.17-051017-generic_5.10.17-051017.202102170631_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.17/amd64/linux-headers-5.10.17-051017_5.10.17-051017.202102170631_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.17/amd64/linux-image-unsigned-5.10.17-051017-generic_5.10.17-051017.202102170631_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.17/amd64/linux-modules-5.10.17-051017-generic_5.10.17-051017.202102170631_amd64.deb
