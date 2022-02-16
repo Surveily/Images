0db8489fca265741eb27b0b6206edbc114a62c45
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2022-02-16 16:08:06 +0100

    add nfs script

---
+++ script/nfs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Run: curl https://raw.githubusercontent.com/Surveily/Images/master/script/nfs.sh | sudo sh
+
+set -e
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+# Install NFS Server
+apt-get update
+apt-get install -y nfs-kernel-server nfs-common
+
+# Configure NFS Server
+mkdir /var/nfs/general -p
+chown nobody:nogroup /var/nfs/general
+echo -e "/var/nfs/general    127.0.0.1(rw,sync,no_subtree_check)" | tee -a /etc/exports
+
+# Restart NFS Server
+systemctl restart nfs-kernel-server
