157c49eef3ca7d652f01d2dc254a01f175bf3ecb
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2022-03-11 15:11:56 +0100

    Create wg0.sh

---
+++ script/wg0.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run: curl https://raw.githubusercontent.com/Surveily/Images/master/script/wg0.sh | sudo sh
+
+# Setup wg0
+
+systemctl enable wg-quick@wg0.service
+systemctl daemon-reload
+systemctl start wg-quick@wg0
+systemctl status wg-quick@wg0
