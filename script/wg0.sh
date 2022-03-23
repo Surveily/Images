b38aa632af6dc372bc395ffd96c64c5219810500
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2022-03-23 14:07:48 +0100

    reresolver

--- script/wg0.sh
+++ script/wg0.sh
@@ -8,3 +8,11 @@ systemctl enable wg-quick@wg0.service
 systemctl daemon-reload
 systemctl start wg-quick@wg0
 systemctl status wg-quick@wg0
+
+# Setup DNS reresolver
+
+wget https://raw.githubusercontent.com/Surveily/Images/master/script/wg0/wireguard_reresolve-dns.service
+wget https://raw.githubusercontent.com/Surveily/Images/master/script/wg0/wireguard_reresolve-dns.timer
+
+mv wireguard_reresolve-dns.service /etc/systemd/system/wireguard_reresolve-dns.service
+mv wireguard_reresolve-dns.timer /etc/systemd/system/wireguard_reresolve-dns.timer
