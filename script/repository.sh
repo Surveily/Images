7efe0c4870ea21f3644d16e40622e31cc9ee8805
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2022-04-11 17:02:53 +0200

    no sudo

--- script/repository.sh
+++ script/repository.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/repository.sh | sudo sh -s -- <password>
+# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/repository.sh | sh -s -- <password>
 
 set -e
 
 HOURS=1000
 PASSWORD=$1
 
-set -- "surveily.init" "surveily.backend.graph" "surveily.cognition.service" "surveily.data.api" "surveily.data.web"
-while [ -n "$1" ]; do
-  echo $1
-  docker run -it --rm anoxis/registry-cli -r https://registry.surveily.com -l surveily:$PASSWORD -i $1 --delete-by-hours $HOURS --keep-tags-like latest
-  shift
+declare -a frequent=(
+    "surveily.init"
+    "surveily.backend.graph"
+    "surveily.cognition.service"
+    "surveily.data.api"
+    "surveily.data.web"
+)
+
+for t in ${frequent[@]}; do
+  echo $t
+  docker run --rm anoxis/registry-cli -r https://registry.surveily.com -l surveily:$PASSWORD -i $t --delete-by-hours $HOURS --keep-tags-like latest
 done
