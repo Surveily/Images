1401b53b2593724328a829f7b74d4cbcefbee967
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-02-06 13:49:47 +0100

    new k3s install script

--- script/k3s-single-new.sh
+++ script/k3s-single-new.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/k3s-single.sh | sudo sh
+# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/k3s-single-new.sh | sudo sh
 
 TXT_YELLOW=`tput setaf 3`
 TXT_NORMAL=`tput sgr0`
@@ -18,17 +18,17 @@ swapoff -a
 sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Install K3S
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy traefik" INSTALL_K3S_VERSION="v1.21.7+k3s1" sh -s - 
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.16+k3s1" sh -s server --cluster-init --disable local-storage --token "surveily"
 systemctl stop k3s
 
 # Configure K3S
 wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/config.toml.tmpl
 wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/config.yaml
-wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/ingress-nginx.yaml
+wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/multipath.conf
 
 mv config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd
 mv config.yaml /etc/rancher/k3s
-mv ingress-nginx.yaml /var/lib/rancher/k3s/server/manifests
+mv multipath.conf /etc/multipath.conf
 
 # Make sure to `vim /etc/rancher/k3s/config.yaml` and `systemctl start k3s` after this script!
-printf "Make sure to: ${TXT_YELLOW}'sudo vim /etc/rancher/k3s/config.yaml'${TXT_NORMAL}\nand start k3s with: ${TXT_YELLOW}'sudo systemctl start k3s'${TXT_NORMAL} after this script!${TXT_NORMAL}\n"
+printf "Make sure to: ${TXT_YELLOW}'vim /etc/rancher/k3s/config.yaml'${TXT_NORMAL}\nand start k3s with: ${TXT_YELLOW}'systemctl start k3s'${TXT_NORMAL} after this script!${TXT_NORMAL}\n"
