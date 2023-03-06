676a1036a824cb166a6462548828f2c512f9cc89
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2023-03-06 10:00:58 +0100

    Update desktop-22.04.sh

--- script/desktop-22.04.sh
+++ script/desktop-22.04.sh
@@ -66,9 +66,9 @@ apt install --fix-broken
 set -e
 
 # Install Lens
-wget -O lens.deb "https://api.k8slens.dev/binaries/latest.amd64.deb"
-dpkg -i lens.deb
-rm lens.deb
+curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
+apt-get && apt-get -y install lens
 
 # Install Mainline
 add-apt-repository ppa:cappelikan/ppa
