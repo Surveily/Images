aed898337aadbd917421c29ffa79a94a6d34feee
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2023-12-10 20:14:15 +0100

    Update node-22.04.sh

--- script/node-22.04.sh
+++ script/node-22.04.sh
@@ -43,7 +43,7 @@ add-apt-repository -y ppa:cappelikan/ppa
 apt-get install -y mainline
 
 # Install QEMU
-apt-get install -y qemu binfmt-support qemu-user-static
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes -c yes
-cat /proc/sys/fs/binfmt_misc/status
-cat /proc/sys/fs/binfmt_misc/qemu-aarch64
+#apt-get install -y qemu binfmt-support qemu-user-static
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes -c yes
+#cat /proc/sys/fs/binfmt_misc/status
+#cat /proc/sys/fs/binfmt_misc/qemu-aarch64
