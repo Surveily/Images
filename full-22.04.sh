ead68413817789ff96b604fdf5fc343e132bc527
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-08 19:55:02 +0200

    Create full-22.04.sh

---
+++ full-22.04.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/full-22.04.sh | bash
+
+FILE=/surveily-init
+
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+else
+   curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/node-22.04.sh | bash
+   reboot
+fi
