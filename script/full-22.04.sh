5594bbce1190089f1db6cd29c847b51bb10b6f93
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-08 20:53:45 +0200

    Update full-22.04.sh

--- script/full-22.04.sh
+++ script/full-22.04.sh
@@ -8,7 +8,7 @@ if [ -f $FILE ]; then
    echo "File $FILE exists."
 else
    curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/node-22.04.sh | bash
-   apt install -y openvpn   
+   apt install -y openvpn
    touch $FILE
    reboot
 fi
