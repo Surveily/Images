7afac98cebfd6e9eca4a2e8483d9b1df54dc5d6c
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-08 20:03:44 +0200

    Update full-22.04.sh

--- script/full-22.04.sh
+++ script/full-22.04.sh
@@ -8,5 +8,6 @@ if [ -f $FILE ]; then
    echo "File $FILE exists."
 else
    curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/node-22.04.sh | bash
+   touch $FILE
    reboot
 fi
