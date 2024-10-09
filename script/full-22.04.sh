84e3010f111b8da4b35116e7c1e6e042414d610c
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-09 11:20:48 +0200

    Update full-22.04.sh

--- script/full-22.04.sh
+++ script/full-22.04.sh
@@ -7,6 +7,7 @@ FILE=/surveily-init
 if [ -f $FILE ]; then
    echo "File $FILE exists."
 else
+   echo "File $FILE not exists."
    curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/node-22.04.sh | bash
    apt install -y openvpn
    wget -O /etc/openvpn/dc.conf https://raw.githubusercontent.com/Surveily/Images/refs/heads/master/script/ovpn/dc.ovpn
@@ -22,6 +23,7 @@ FILE=/surveily-k3s
 if [ -f $FILE ]; then
    echo "File $FILE exists."
 else
+   echo "File $FILE not exists."
    curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/k3s-multi-agent.sh | sh -s -- 10.100.1.10
    touch $FILE
    reboot
