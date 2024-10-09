d95a13f84a1f7bd2441f1db18d516fef64a59351
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-09 11:08:13 +0200

    Update k3s-multi-agent.sh

--- script/k3s-multi-agent.sh
+++ script/k3s-multi-agent.sh
@@ -18,12 +18,9 @@ swapoff -a
 sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Configure
-wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/config.yaml
-wget https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/multipath.conf
-
 mkdir -p /etc/rancher/k3s
-mv config.yaml /etc/rancher/k3s
-mv multipath.conf /etc/multipath.conf
+wget -O /etc/rancher/k3s/config.yaml https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/config.yaml
+wget -O /etc/rancher/k3s/multipath.yaml https://raw.githubusercontent.com/Surveily/Images/master/script/k3s/multipath.conf
 
 # Install K3S
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.27.1+k3s1" K3S_TOKEN="surveily" sh -s agent --server https://$1:6443
