fcd6212b0ba3a1b85cf3480f328880c2348ab8d7
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-10-11 13:50:17 +0200

    Update k3s-wg-join-agent.sh

--- script/k3s-wg-join-agent.sh
+++ script/k3s-wg-join-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/k3s-multi-agent.sh | sudo sh -s -- <ip> <ip>
+# Run: curl -s https://raw.githubusercontent.com/Surveily/Images/master/script/k3s-wg-join-agent.sh | sudo sh -s -- <ip> <ip>
 
 TXT_YELLOW=`tput setaf 3`
 TXT_NORMAL=`tput sgr0`
