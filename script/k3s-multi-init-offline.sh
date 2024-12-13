ee5c8fe643608e785a5fba416e4ccf168b30b42e
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2024-12-13 16:19:43 +0100

    Update k3s-multi-init-offline.sh

--- script/k3s-multi-init-offline.sh
+++ script/k3s-multi-init-offline.sh
@@ -19,8 +19,8 @@ sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Configure K3S
 mkdir -p /etc/rancher/k3s
-mv config.yaml /etc/rancher/k3s/config.yaml
-mv multipath.conf /etc/multipath.conf
+cp config.yaml /etc/rancher/k3s/config.yaml
+cp multipath.conf /etc/multipath.conf
 
 # Install K3S
 INSTALL_K3S_VERSION="v1.27.1+k3s1" INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC='server --disable local-storage --cluster-init --token=surveily' ./install.sh
