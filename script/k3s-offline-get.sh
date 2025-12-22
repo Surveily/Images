512e9cd73553a57bb86fcd8410a444cccb9d3867
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2025-12-22 12:09:11 +0100

    Update k3s-offline-get.sh

--- script/k3s-offline-get.sh
+++ script/k3s-offline-get.sh
@@ -14,8 +14,8 @@ fi
 
 mkdir -p .out-k3s
 
-wget -O .out-k3s/k3s https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s
-wget -O .out-k3s/k3s-airgap-images-amd64.tar.gz https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s-airgap-images-amd64.tar.gz
+wget -O .out-k3s/k3s https://github.com/k3s-io/k3s/releases/download/v1.34.2%2Bk3s1/k3s
+wget -O .out-k3s/k3s-airgap-images-amd64.tar.gz https://github.com/k3s-io/k3s/releases/download/v1.34.2%2Bk3s1/k3s-airgap-images-arm64.tar.gz
 wget -O .out-k3s/install.sh https://get.k3s.io/
 
 chmod +x .out-k3s/k3s
