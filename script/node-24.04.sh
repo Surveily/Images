cac305dba200f208137bb8354e609fb59aac3fec
Wojtek Turowicz <wojtek.turowicz@gmail.com>
2026-01-20 11:54:16 +0100

    Update node-24.04.sh

--- script/node-24.04.sh
+++ script/node-24.04.sh
@@ -43,7 +43,7 @@ apt-get remove -y unattended-upgrades
 curl https://get.docker.com | sh && systemctl --now enable docker
 
 # Install Drivers
-apt-get -y install --no-install-recommends cuda-drivers-575
+apt-get -y install --no-install-recommends cuda-drivers-580
 
 # Install Nvidia-Docker
 apt-get install -y nvidia-container-toolkit
